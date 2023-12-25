3cb11f0782224382b77b9f657d6ed74d021aaeed
ilya <ilytoma@mail.ru>
2023-12-25 13:39:23 +0300

    Add class Animals, Pets and Enum PetsType

---
+++ JavaProject/src/main/java/org/example/Model/Animals.java
@@ -0,0 +1,14 @@
+package org.example.Model;
+
+import java.util.List;
+
+abstract class Animals {
+    private String Name;
+    private String BirthDate;
+    private List<String> Commands;
+
+    public Animals(String name, String birthDate, List<String> commands) {
+    }
+
+
+}
